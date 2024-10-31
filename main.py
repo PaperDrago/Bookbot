e8f22164bf3672f5f608b29b5fa3a9160e936aff
PaperDrago <charlie_shadow@hotmail.com>
2024-10-31 15:30:36 -0500

    finished the static book genorator report

--- main.py
+++ main.py
@@ -2,9 +2,27 @@ def main():
     book_path = "books/frankenstein.txt"
     text = bookText(book_path)
     word_count = get_num_words(text)
-    print(f"This is how many words are in the text: {word_count}\n")
     character_count = count_characters(text)
-    print(f"There are this many character in the text: {character_count}\n")
+    list_dict = convert_dict_to_list(character_count)
+    #print(f"There are this many character in the text: {character_count}\n")
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{word_count} words found in the document\n")
+    for dictionary in list_dict:
+        print(f"The '{dictionary['char']}' character was found {dictionary['num']} times")
+
+
+def convert_dict_to_list(dictionary):
+    list_dicts = []
+    for dict in dictionary:
+        temp = {}
+        temp["char"] = dict
+        temp["num"] = dictionary[dict]
+        list_dicts.append(temp)
+    list_dicts.sort(reverse=True, key=sort_on)
+    return list_dicts
+
+def sort_on(dict):
+    return dict["num"]
 
 
 def bookText(bookPath):
